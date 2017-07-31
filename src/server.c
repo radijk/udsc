e9e93997ec05b878d5a44cc1a50c1d7bcd17de39
rad <rad.ra@protonmail.com>
2017-07-31 22:39:11 +0200

    Catch signals SIGINT / SIGTERM and exit properly
    Signed-off-by: rad <rad.ra@protonmail.com>

--- src/server.c
+++ src/server.c
@@ -4,22 +4,27 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/epoll.h>
+#include <sys/signalfd.h>
+#include <signal.h>
 #include <errno.h>
 #include <err.h>
 #include "udsc.h"
 #include "utils.h"
 #include "server.h"
 
-#define MAX_EVENTS	2
+#define MAX_EVENTS	3
 
 int server(struct sockinfo *sockinfo)
 {
-	int sockfd, clifd;
+	int sockfd, clifd, sigfd;
 	int epfd, nfds, n;
 	ssize_t count;
 	char buf[BUFSIZ];
 	struct sockaddr_un addr = {0};
-	struct epoll_event event, events[MAX_EVENTS] = {0};
+	struct epoll_event event = {0}, events[MAX_EVENTS] = {0};
+	sigset_t sigmask;
+	struct signalfd_siginfo fdsi;
+	size_t size = BUFSIZ;
 
 	epfd = epoll_create1(0);
 	if (epfd == -1) {
@@ -75,25 +80,49 @@ int server(struct sockinfo *sockinfo)
 		goto err_client;
 	}
 
+	sigemptyset(&sigmask);
+	sigaddset(&sigmask, SIGINT);
+	sigaddset(&sigmask, SIGTERM);
+
+	if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1) {
+		warn("sigprocmask failed");
+		goto err_client;
+	}
+
+	sigfd = signalfd(-1, &sigmask, SFD_NONBLOCK);
+	if (sigfd == -1) {
+		warn("signalfd failed");
+		goto err_client;
+	}
+
+	event.events = EPOLLIN | EPOLLET;
+	event.data.fd = sigfd;
+
+	if (epoll_ctl(epfd, EPOLL_CTL_ADD, sigfd, &event)) {
+		warn("epoll_ctl failed");
+		goto err_sig;
+	}
+
+
 	while (1) {
 		nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
 		if (nfds == -1) {
 			warn("epoll_wait failed");
-			goto err_client;
+			goto err_sig;
 		}
 		for (n = 0; n < nfds; n++) {
 			if (events[n].data.fd == clifd) {
 				if (events[n].events &
 				   (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
 					print("client disconnected\n");
-					goto err_client;
+					goto err_sig;
 				}
 				if (events[n].events & EPOLLIN) {
 					memset(buf, 0, BUFSIZ);
 					count = read(clifd, buf, BUFSIZ);
 					if (count == -1) {
 						warn("read failed");
-						goto err_client;
+						goto err_sig;
 					}
 					print("%s", buf);
 				}
@@ -104,20 +133,38 @@ int server(struct sockinfo *sockinfo)
 							sockinfo->eol);
 					if (count == -1) {
 						warn("getinput failed");
-						goto err_client;
+						goto err_sig;
 					}
 					count = write(clifd, buf, count);
 					if (count == -1) {
 						warn("write failed");
-						goto err_client;
+						goto err_sig;
+					}
+				}
+			}
+			if (events[n].data.fd == sigfd) {
+				if (events[n].events & EPOLLIN) {
+					size = sizeof(struct signalfd_siginfo);
+					count = read(sigfd, &fdsi, size);
+					if (count == -1) {
+						warn("read failed");
+						goto err_sig;
+					}
+					if ((fdsi.ssi_signo == SIGINT) ||
+					(fdsi.ssi_signo == SIGTERM)) {
+						closew(sigfd);
+						closew(clifd);
+						closew(sockfd);
+						closew(epfd);
+						return 0;
 					}
 				}
 			}
 		}
 	}
 
-	return 0;
-
+err_sig:
+	closew(sigfd);
 err_client:
 	closew(clifd);
 err_sock:
